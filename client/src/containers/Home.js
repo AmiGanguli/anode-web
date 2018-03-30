e3daeb2d3f526c05cd0d37ec08231a1da58debf8
Cloud User <ec2-user@ip-172-30-0-10.ec2.internal>
2018-03-30 22:22:55 +0000

    Home page.

---
+++ client/src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Anode</h1>
+          <p>Collaborative Analytics</p>
+        </div>
+      </div>
+    );
+  }
+}
+
