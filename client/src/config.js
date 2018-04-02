acb14407a28ea119aff6299960479e843105dfba
Cloud User <ec2-user@ip-172-30-0-10.ec2.internal>
2018-04-02 17:36:35 +0000

    Basic login.

---
+++ client/src/config.js
@@ -0,0 +1,17 @@
+export default {
+	s3: {
+		REGION: "us-east-1",
+		BUCKET: "ganguli.com-anode-web-uploads"
+	},
+	apiGateway: {
+		REGION: "us-east-1",
+		URL: "https://qti4nfhq6c.execute-api.us-east-1.amazonaws.com/prod"
+	},
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_aP4u8Ma1F",
+		APP_CLIENT_ID: "12tke65ks3ff1f0dn0khdbmk66",
+		IDENTITY_POOL_ID: "us-east-1:a35342a3-6a05-436a-a798-192f170a60a3"
+	}
+};
+
