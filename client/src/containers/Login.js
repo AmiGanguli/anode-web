cda9e7678b1761e6e171aee68556162ec6150ac9
Cloud User <ec2-user@ip-172-30-0-10.ec2.internal>
2018-04-10 21:25:13 +0000

    Home page lists notes.

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import {Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
+import {FormGroup, FormControl, ControlLabel} from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import {Auth} from "aws-amplify";
 import "./Login.css";
 
@@ -7,6 +8,7 @@ export default class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			isLoading: false,
 			email: "",
 			password: ""
 		};
@@ -21,11 +23,14 @@ export default class Login extends Component {
 	}
 	handleSubmit = async event => {
 		event.preventDefault();
+		this.setState({isLoading: true});
 		try {
 			await Auth.signIn(this.state.email, this.state.password);
-			alert("Logged in");
+			this.props.userHasAuthenticated(true);
+			this.props.history.push("/");
 		} catch (e) {
 			alert(e.message);
+			this.setState({isLoading: false});
 		}
 	}
 	render() {
@@ -49,14 +54,15 @@ export default class Login extends Component {
 							type="password"
 						/>
 					</FormGroup>
-					<Button
+					<LoaderButton
 						block
 						bsSize="large"
 						disabled={!this.validateForm()}
 						type="submit"
-					>
-						Login
-					</Button>
+						isLoading={this.state.isLoading}
+						text="Login"
+						loadingText="Logging in..."
+					/>
 				</form>
 			</div>
 		);
